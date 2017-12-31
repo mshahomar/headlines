a682894cbc5474217c148afdf1b472508a4911e5
mshah.omar <mshahomar@gmail.com>
2017-12-31 11:39:43 +0800

    Added OpenWeatherMap API

--- headlines.py
+++ headlines.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 import feedparser
+import json
+from urllib import parse as url_parse
+from urllib import request as url_request
 
 
 app = Flask(__name__)
@@ -18,54 +21,55 @@ NEWS_FEEDS = {
     "FMT": "http://www.freemalaysiatoday.com/feed/"
 }
 
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=a5ebfde13305fd3c4a6ef3d2bef29a47"
+DEFAULTS = {"city": "Kuala Selangor", "publication": "MKINI_TERKINI"}
+
 publishers = list(publisher.title() for publisher in  NEWS_FEEDS.keys())
 
 
+@app.route("/")
+def home():
+    # Get News Publication
+    publication = request.args.get("publication")
+    if not publication:
+        publication = DEFAULTS["publication"]
+    articles = get_news(publication)
+
+    # Get City for weather data
+    city = request.args.get("city")
+    if not city:
+        city = DEFAULTS["city"]
+    weather = get_weather(city)
+
+    return render_template("index.html", articles=articles, weather=weather)
+
 
 @app.route("/")
-@app.route("/mkini/terkini")
-def mkini_terkini():
-    return get_news("MKINI_TERKINI")
-
-@app.route("/mkini/kolumnis")
-def mkini_kolumnis():
-    return get_news("MKINI_KOLUMNIS")
-
-@app.route("/thestar/editor")
-def thestar_editor():
-    return get_news("THESTAR_EDITOR")
-
-@app.route("/thestar/nation")
-def thestar_nation():
-    return get_news("THESTAR_NATION")
-
-@app.route("/tmi/en")
-def tmi_english():
-    return get_news("TMI_ENGLISH")
-
-@app.route("/tmi/bm")
-def tmi_bahasa():
-    return get_news("TMI_BAHASA")
-
-@app.route("/fmt")
-def fmt():
-    return get_news("FMT")
-
-
-def get_news(publication):
-    query = request.args.get("publication")
-    print(query)
-    if not query or query not in NEWS_FEEDS:
-        publication
+def get_news(search_query):
+    if not search_query or search_query not in NEWS_FEEDS:
+        publication = DEFAULTS["publication"]
     else:
-        publication = query
-    
+        publication = search_query.upper()
     feed = feedparser.parse(NEWS_FEEDS[publication])
-    articles = feed['entries']
-    channel = feed['channel']['title']
+        
+    return feed['entries']
     
-    return render_template("index.html", articles=articles, channel=channel)
+def get_weather(query):
+    query = url_parse.quote(query)
+    url = WEATHER_URL.format(query)
+    data = url_request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+
+    if parsed.get("weather"):
+        weather = {"description": parsed["weather"][0]["description"], 
+                  "temperature": parsed["main"]["temp"], 
+                  "city": parsed["name"],
+                  "country": parsed["sys"]["country"]
+                  }
     
+    return weather
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
